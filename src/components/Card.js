b9e457738fbcc78ea8e80a0f993a09d90298a8ab
Роман_Ходус <khodus.r@yandex.ru>
2022-07-30 10:03:14 +0700

    feature use context to delete and like button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,19 @@
+import React from "react";
+import { CurrentUserContext } from "../context/CurrentUserContext.js";
+
 function Card({ card, onCardClick }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const cardDeleteButtonClassName = `elements__delete ${
+    isOwn ? "elements__delete_visible" : "elements__delete_hidden"
+  }`;
+
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const cardLikeButtonClassName = `elements__like ${isLiked ? "elements__like_active" : ""}`;
+
   function handleClick() {
     onCardClick(card);
-  } 
+  }
 
   return (
     <article className="elements__card">
@@ -13,11 +25,11 @@ function Card({ card, onCardClick }) {
           onClick={handleClick}
         />
       </div>
-      <button type="button" className="elements__delete"></button>
+      <button type="button" className={cardDeleteButtonClassName}></button>
       <div className="elements__figcaption">
         <h2 className="elements__header">{card.name}</h2>
         <div className="elements__like-container">
-          <button type="button" className="elements__like"></button>
+          <button type="button" className={cardLikeButtonClassName}></button>
           <div className="elements__like-counter">{card.likes.length}</div>
         </div>
       </div>
